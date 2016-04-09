ff6941517bfa42bdaf2711ff67ec69f4cc712fba
delventhalz <delventhalz@gmail.com>
2016-04-08 23:16:58 -0500

    Draft of the extra credit problems

---
+++ 04.ExtraCredit.js
@@ -0,0 +1,27 @@
+/* * * * * * * * * * * * * * * * * * * * * *
+ *     CAVEMAN JS PT 4: EXTRA CREDIT       *
+ * * * * * * * * * * * * * * * * * * * * * */
+
+// Looking for more key JavaScript tools to implement? These are a bit 
+// harder than the ones that came before, but I know you can do it.
+
+
+// `Array.prototype.splice` allows you to modify an existing array by
+// removing a piece of an array and inserting any number of new elements.
+var splice = function(array, start, end) {
+
+};
+
+
+// `Date.parse` is an interesting function which takes a date string 
+// (with or without a time) and returns the number of milliseconds
+// since January 1, 1970, the Unix epoch.
+var parse = function(date) {
+
+};
+
+
+// `Math.sqrt` Good luck.
+var sqrt = function(number) {
+
+};
