a9f11ff70b71db24c7b6aa46e72b22fe922c70b0
delventhalz <delventhalz@gmail.com>
2016-04-08 22:13:46 -0500

    Draft of third section, String.prototype

---
+++ 03.String.js
@@ -0,0 +1,37 @@
+/* * * * * * * * * * * * * * * * * * * * * *
+ *   CAVEMAN JS PT 3: STRING.PROTOTYPE     *
+ * * * * * * * * * * * * * * * * * * * * * */
+
+// Primitive values can get in on the method madness too! 
+
+
+// `String.prototype.slice` is a phenomenally useful method that copies
+// a piece of a string based on a start index (inclusive) and an end 
+// index (non-inclusive). The `Array.prototype` has a very similar 
+// method. Design your `slice` to work for both strings and arrays.
+var slice = function(stringOrArray, start, end) {
+
+};
+
+
+// `String.prototype.split` can return an array based on a string by 
+// breaking it up using a delimiter string. The delimiter is not 
+// included in the final array.
+var split = function(string) {
+
+};
+
+
+// `String.prototype.trim` is a handy little method for processing text.
+// It returns the input string with all white space removed from the
+// beginning and the end. 
+var trim = function(string) {
+
+};
+
+
+// `String.prototype.replace` modifies a string by searching for a target
+// string and replacing it. Note that only the first instance is replaced.
+var replace = function(string, target, replacement) {
+
+};
